850df3e38fe39ca76e7db6a5c14b38e6d1973605
madhu.narula@nasdaq.com <madhu.narula@nasdaq.com>
2021-07-13 21:26:37 +0000

    wokring-session#1--add customer lifetime_value

--- models/customers.sql
+++ models/customers.sql
@@ -6,7 +6,7 @@ with customers as (
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('orders') }}
 
 ),
 
@@ -17,8 +17,8 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
 
     group by 1
